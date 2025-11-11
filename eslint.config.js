ec4e386623236532b8f19ccf28411a97e7e5f041
M Komar <45166197+Zappp@users.noreply.github.com>
2025-11-11 15:43:13 +0100

    Clean up eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -32,27 +32,3 @@ export default [
   // Keep this last to disable any rules that conflict with Prettier
   prettier
 ]
-
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-])
